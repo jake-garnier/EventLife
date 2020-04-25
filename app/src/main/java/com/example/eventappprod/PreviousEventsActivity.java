614d016e228466b736fde3f043c9b8820347dd40
vincentxtran <vptran@ucsd.edu>
2020-04-25 16:06:28 -0700

    made progres on profile; todo: unique eventactivity pages, unique adapters, but working concept

---
+++ app/src/main/java/com/example/eventappprod/PreviousEventsActivity.java
@@ -0,0 +1,33 @@
+package com.example.eventappprod;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class PreviousEventsActivity extends AppCompatActivity {
+    //https://www.youtube.com/watch?v=18VcnYN5_LM
+    //Event Feed String Arrays
+    String eventNames[];
+    String eventDescriptions[];
+    int images[] = {R.drawable.revelle, R.drawable.muir, R.drawable.tmc, R.drawable.warren, R.drawable.erc, R.drawable.sixth, R.drawable.samoyed, R.drawable.khosla};
+
+    //Recycler View Needed for Event Feed
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_previous_events);
+
+        //Logic for displaying the event-feed
+        //Thus, somehow inject database informaiton into these arrays?
+        recyclerView = findViewById(R.id.prevEventRecycler);
+        eventNames = getResources().getStringArray(R.array.eventNames_feed);
+        eventDescriptions = getResources().getStringArray(R.array.eventNames_description);
+        MyAdapter myAdapter = new MyAdapter(this, eventNames, eventDescriptions, images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
--- app/src/main/java/com/example/eventappprod/Profile.java
+++ app/src/main/java/com/example/eventappprod/Profile.java
@@ -6,15 +6,57 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Profile extends AppCompatActivity {
 
+    //Buttons
+    private Button friendsListButton;
+    private Button prevEventButton;
+    private Button archivedEventButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        //Button Stuff
+        friendsListButton = (Button) findViewById(R.id.viewFriendsButton);
+        friendsListButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext()
+                        ,FriendsListActivity.class));
+
+            }
+
+        });
+
+        prevEventButton = (Button) findViewById(R.id.viewHistoryButton);
+        prevEventButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext()
+                        ,PreviousEventsActivity.class));
+
+            }
+
+        });
+
+        archivedEventButton = (Button) findViewById(R.id.viewArchiveButton);
+        archivedEventButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext()
+                        ,ArchivedEventsActivity.class));
+
+            }
+
+        });
+
 
         //Initialize and assign variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
