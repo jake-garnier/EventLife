ce1b45f7b55100e05f356692c35af089e05efe44
LK100 <56284212+LK100@users.noreply.github.com>
2020-05-24 15:48:58 -0700

    Update Event.java

--- app/src/main/java/com/example/eventappprod/Event.java
+++ app/src/main/java/com/example/eventappprod/Event.java
@@ -27,16 +27,17 @@ public class Event {
     public Event(String id,String name, String date, String loca,
                  String stime, String etime, String tag, String des,
                  String usergo, String usermaybe, String usernot) {
+        // mandatory fields for an event object
         ID = id;
         Name = name;
         Date = date;
         Location = loca;
-
         Tag =tag;
         StartTime =stime;
         EndTime = etime;
         Description = des;
 
+        // field filled by user's action
         userGoing = usergo;
         userMaybeGoing = usermaybe;
         userNotGoing = usernot;
@@ -104,21 +105,24 @@ public class Event {
         return userGoing;
     }
     public void addUserGoing(String g){
-        userGoing = userGoing.concat(g);
+        userGoing = userGoing.concat(g+ "$");// use '$' to parse the users since
+        // Real-time Firebase database can store data under String objects only
     }
 
     public String getUserMaybeGoing(){
         return userMaybeGoing;
     }
     public void addUserMaybeGoing(String m){
-        userMaybeGoing = userMaybeGoing.concat(m);
+        userMaybeGoing = userMaybeGoing.concat(m+ "$");// use '$' to parse the users since
+        // Real-time Firebase database can store data under String objects only
     }
 
     public String getUserNotGoing(){
         return userNotGoing;
     }
     public void addUserNotGoing(String n){
-        userNotGoing = userNotGoing.concat(n);
+        userNotGoing = userNotGoing.concat(n+ "$");// use '$' to parse the users since
+        // Real-time Firebase database can store data under String objects only
     }
 
 }
