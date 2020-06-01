dcfd5ac5669d5ecf83d33bc7c95b1cd9da8dcf5a
yvonne chen <yvchen@ucsd.edu>
2020-06-01 02:12:33 -0700

    deleted that toast?
    Toast and butter

--- app/src/main/java/com/example/eventappprod/CreateEventActivity.java
+++ app/src/main/java/com/example/eventappprod/CreateEventActivity.java
@@ -237,7 +237,6 @@ public class CreateEventActivity extends AppCompatActivity {
 
                 if(!RealTimeImagePath.isEmpty() && create)
                 {
-                    Toast.makeText(CreateEventActivity.this, "Debug adding purpose", Toast.LENGTH_LONG).show();
                     addEvent();
                     Toast.makeText(CreateEventActivity.this, "Event created", Toast.LENGTH_LONG).show();
                     startActivity(new Intent(getApplicationContext(), DashBoard.class));
