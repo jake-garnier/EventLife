a3f886ccf5e5ad73661c39f9662a5dfce6e3e33a
cpp1997 <50185358+cpp1997@users.noreply.github.com>
2020-06-04 16:20:32 -0700

    Edited register

--- app/src/main/java/com/example/eventappprod/Register.java
+++ app/src/main/java/com/example/eventappprod/Register.java
@@ -116,7 +116,7 @@ public class Register extends AppCompatActivity {
                                         mEmail.setText("");
                                         mPassword.setText("");
                                         u  = new User(fullName, email, "",password, "", "https://firebasestorage.googleapis.com/v0/b/event-b161b.appspot.com/o/EVENT%2Fraw%253A%252Fstorage%252Femulated%252F0%252FDownload%252Fdownload.png?alt=media&token=06256f12-4b6f-4099-84f0-8aa4a90f0159",
-                                                "https://firebasestorage.googleapis.com/v0/b/event-b161b.appspot.com/o/EVENT%2F285871589?alt=media&token=0a2f3f7e-e6f8-4b44-ac9b-b15d0669ecd1","", "", "", "FootLocker Meetup", "", "j2chong,kcpham,");
+                                                "https://firebasestorage.googleapis.com/v0/b/event-b161b.appspot.com/o/EVENT%2F285871589?alt=media&token=0a2f3f7e-e6f8-4b44-ac9b-b15d0669ecd1","", "", "", "", "", "");
                                         usercount++;
                                         String userRef = Integer.toString(usercount);
                                         String userID = email.substring(0, email.indexOf("@"));
