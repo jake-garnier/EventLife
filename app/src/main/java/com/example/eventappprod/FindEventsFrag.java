f924ddcdddd78146031a3b049518a5ee078f00e9
yvonne chen <yvchen@ucsd.edu>
2020-06-05 03:32:42 -0700

    added (add friend function in find event frag
    :D button works

--- app/src/main/java/com/example/eventappprod/FindEventsFrag.java
+++ app/src/main/java/com/example/eventappprod/FindEventsFrag.java
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -13,6 +16,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -33,15 +37,26 @@ public class FindEventsFrag extends Fragment {
     String[] eventEndTime_Screenshow=new String[20];
     String[] eventDate_Screenshow=new String[20];
 
-    String[] friendList = new String[20];
-
     //Recycler View Needed for Event Feed
     private RecyclerView mRecyclerView;
     private ExampleAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     DatabaseReference ref;
-    User currUser  = User.getInstance();
+    DatabaseReference userRef;
     ArrayList<Event> evenList;
+
+    //Current user information
+    ArrayList<User> userList;
+    User currUser  = User.getInstance();
+    String userID = currUser.getUserId();
+    String[] friendList;
+    String[] userIDList = new String[20];
+
+    //Current user's friendslist
+    String[] friendArr;
+    String friendAdd;
+    int added = 0;
+
 
     @Nullable
     @Override
@@ -51,6 +66,33 @@ public class FindEventsFrag extends Fragment {
 
         evenList = new ArrayList<>();
         ref = FirebaseDatabase.getInstance().getReference("/EVENT");
+
+        userList = new ArrayList<>();
+        userRef = FirebaseDatabase.getInstance().getReference("/USER");
+
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                userList.clear();
+                //fetch data from snap shots from database to populate array
+                for (DataSnapshot ds : dataSnapshot.getChildren()){
+                    userList.add(ds.getValue(User.class));
+                }
+                for (int i=0; i < userList.size() ;i++) {
+                    if(userList.get(i).getUserId().equals(userID)){
+                        currUser = userList.get(i);
+                    }
+                }
+                //check if data is fully fetched
+                if (userList.size()!=0) {
+                    retrieveData(view);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(FindEventsFrag.super.getContext(), "Error on Firebase", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         friendList = currUser.getFriendList().split(",");
 
@@ -68,22 +110,17 @@ public class FindEventsFrag extends Fragment {
                     if (ds.child("owner").getValue().equals(currUser.getUserId() + ",")) {
                         flag = false;
                     }
-
-
                     String[] rsvpevents = currUser.getRSVPEvents().split(",");
 
-                    for(String e : rsvpevents) {
-
+                    for (String e : rsvpevents) {
                         if (ds.child("name").getValue().equals(e)) {
                             flag = false;
                         }
-
                     }
-                    if(flag) {
+                    if (flag) {
                         evenList.add(ds.getValue(Event.class));
                     }
                 }
-
                 if (evenList.size() != 0) {
                     retrieveData(view);
                 }
@@ -94,15 +131,17 @@ public class FindEventsFrag extends Fragment {
                 Toast.makeText(FindEventsFrag.super.getContext(), "Error on Firebase", Toast.LENGTH_SHORT).show();
             }
         });
-
         return view;
     }
 
     public void retrieveData(View view){
 
-        // fetching data to particular array
+        // fetching data to arrays relating to users
+        for (int i=0; i < userList.size();i++) {
+            userIDList[i] = userList.get(i).getUserId();
+        }
 
-        // fetching data to particular array
+        // fetching data to arrays relating to events
         for (int i=0; i<evenList.size();i++) {
             eventNames_Screenshow[i] = evenList.get(i).getName();
             eventStartTime_Screenshow[i] = evenList.get(i).getStartTime();
@@ -110,7 +149,6 @@ public class FindEventsFrag extends Fragment {
             eventDate_Screenshow[i] = evenList.get(i).getDate();
             images_Firestore[i] = evenList.get(i).getImage();
         }
-
         LoadDatatoDashBoard(view);
 
     }
@@ -122,20 +160,90 @@ public class FindEventsFrag extends Fragment {
                     eventEndTime_Screenshow[i], eventDate_Screenshow[i], "", images_Firestore[i]));
         }
 
+        friendArr = currUser.getFriendList().split(",");
+
         mRecyclerView = view.findViewById(R.id.recyclerViewFindEvents);
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(this.getContext());
         mAdapter = new ExampleAdapter(this.getContext(), exampleList, "");
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
-
     }
 
+    //the method for adding friends in friend fragment based on the button in the menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.add_friend_button) {
+            //create the alert box
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle("Friend Request? Add their Username below and zoom!");
+
+            // Set up the input
+            final EditText input = new EditText(getActivity());
+            // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+            input.setPaddingRelative(40,20,20,20);
+            input.setInputType(InputType.TYPE_CLASS_TEXT);
+            builder.setView(input);
+
+            // Used when current user presses the add button in the alert box
+            builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    friendAdd = input.getText().toString();
+                    boolean flag = false;
+                    if(userList.size()!=0)
+                    {
+                        for (int i = 0; i < userList.size(); i++) {
+                            flag = false;
+                            for (int j = 0; j < friendArr.length;j++) {
+                                //checks if the user exists in the database or not (aka spelling errors)
+                                if ((userList.get(i).getUserId().equals(friendArr[j]))
+                                        || userList.get(i).getUserId().equals(currUser.getUserId())) {
+                                    flag = true;
+                                }
+                                //checks if the friend can be added to the friend list
+                                if (userList.get(i).getUserId().equals(friendAdd) && flag == false)
+                                {
+                                    currUser.addFriend(friendAdd);
+                                    //sets new friends list into the database
+                                    userRef.child(currUser.getUserId()).child("friendList").setValue(currUser.getFriendList());
+                                    added = 1;
+                                    break;
+                                }
+                            }
+                        }
+                        //displays the correct toast message depending on if the person was added or not
+                        if (added == 1) {
+                            Toast.makeText(getActivity(), "Added : " + friendAdd, Toast.LENGTH_SHORT).show();
+                            added = 0;
+                        } else {
+                            Toast.makeText(getActivity(), friendAdd + " : Does not exist or Already added", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    dialog.cancel();
+                }
+            });
+            //used when current user decides to stop the process of adding the friend
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            builder.show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //this method makes sure we are using the correct menu display on the top of the app
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
+        //select the correct menu layout
         inflater.inflate(R.menu.example_menu, menu);
 
+        //utilizes the search action
         MenuItem searchItem =  menu.findItem(R.id.action_search);
         final androidx.appcompat.widget.SearchView searchView = (androidx.appcompat.widget.SearchView) searchItem.getActionView();
         searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
@@ -146,9 +254,7 @@ public class FindEventsFrag extends Fragment {
             }
             @Override
             public boolean onQueryTextChange(String newText) {
-                if(evenList.size() != 0) {
-                    mAdapter.getFilter().filter(newText);
-                }
+                mAdapter.getFilter().filter(newText);
                 return false;
             }
         });
--- app/src/main/java/com/example/eventappprod/FindFriendsFrag.java
+++ app/src/main/java/com/example/eventappprod/FindFriendsFrag.java
@@ -34,35 +34,30 @@ import javax.annotation.Nullable;
 
 public class FindFriendsFrag extends Fragment {
 
-    FirebaseStorage storage = FirebaseStorage.getInstance();
-    StorageReference gsRef;
-
-    String[] profilePic = new String[20];
-    String[] userName = new String[20];
-    String[] userIDList = new String[20];
-
     //Recycler View Needed for Event Feed
     private RecyclerView mRecyclerView;
     private ExampleAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
-    FirebaseDatabase database;
+
+    //Database variables and lists needed to pull data from firebase
     DatabaseReference ref;
     ArrayList<User> userList;
     ArrayList<ExampleItem> exampleList = new ArrayList<>();;
     ArrayList<User> newUserList;
 
-    //current User stuff
-    String[] friendArr;
+    //arrays used to make sure database populates the arrays
+    String[] profilePic = new String[20];
+    String[] userName = new String[20];
+    String[] userIDList = new String[20];
+
+    //Current user information
     User currUser = User.getInstance();
     String userID = currUser.getUserId();
 
-    //friends stuff
+    //Current user's friendslist
+    String[] friendArr;
     String friendAdd;
     int added = 0;
-    private String[] array;
-
-
-    private Button btnTEST;
 
     @Nullable
     @Override
@@ -70,6 +65,7 @@ public class FindFriendsFrag extends Fragment {
         final View view = inflater.inflate(R.layout.fragment1_find_friends, container, false);
         setHasOptionsMenu(true);
 
+        //setting up the recycler view and adapter
         mRecyclerView = view.findViewById(R.id.recyclerViewFindFriends);
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(this.getContext());
@@ -77,23 +73,26 @@ public class FindFriendsFrag extends Fragment {
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
 
+        //populate the information from database to local arraylist
         userList = new ArrayList<>();
-        ref = FirebaseDatabase.getInstance().getReference("/USER");
         newUserList = new ArrayList<>();
+        ref = FirebaseDatabase.getInstance().getReference("/USER");
         ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 userList.clear();
                 newUserList.clear();
+                //fetch data from snap shots from database to populate array
                 for (DataSnapshot ds : dataSnapshot.getChildren()){
                     newUserList.add(ds.getValue(User.class));
                 }
+                //check if data is fully fetched
                 if (newUserList.size()!=0) {
                     userList = newUserList;
                     retrieveData(view);
                 }
             }
-
+            // checks if there is an error retrieving data from database
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 Toast.makeText(FindFriendsFrag.super.getContext(), "Error on Firebase", Toast.LENGTH_SHORT).show();
@@ -103,61 +102,60 @@ public class FindFriendsFrag extends Fragment {
         return view;
     }
 
+    //to allow data to be extracted fully from database
     public void retrieveData(View view){
-
         // fetching data to particular array
-
         for (int i=0; i < userList.size();i++) {
             userIDList[i] = userList.get(i).getUserId();
             userName[i] = userList.get(i).getName();
             profilePic[i] = userList.get(i).getProfileImage();
-            // you can get other info like date and time as well
-            //Bitmap my_image;
-            //Picasso.get().load(evenList.get(i).getImage()).into(my_image);
         }
+        //fetching current user from the list of users in database
         for (int i=0; i < userList.size() ;i++) {
             if(userList.get(i).getUserId().equals(userID)){
                 currUser = userList.get(i);
             }
         }
-
         LoadDatatoDashBoard(view);
 
     }
 
+    //populate the fragment with users that aren't your friend
     public void LoadDatatoDashBoard(View view){
+        //get user's friends list
         friendArr = currUser.getFriendList().split(",");
         User userCompare = new User();
-
         boolean flag = false;
-
+        //iterate through the entire user list from database
         for (int i = 0; i < userList.size(); i++) {
             flag = false;
+            //iterate through the current user's friendlist
             for(int j = 0; j < friendArr.length; j++) {
+                //check if the user is already in the friends list or the user itself
                 if ((userList.get(i).getUserId().equals(friendArr[j]))
                         || userList.get(i).getUserId().equals(currUser.getUserId())) {
                     flag = true;
                 }
             }
+            //if the user isn't the current user or part of the current user's friend list display it
             if (flag == false) {
                 userCompare = userList.get(i);
+                //populate the card and add it in the fragment
                 exampleList.add(new ExampleItem(userCompare.getName(), userCompare.getUserId(),
                         "", "", "", userCompare.getProfileImage()));
                 mAdapter.notifyItemInserted(0);
                 mAdapter.resetFull();
                 mRecyclerView.scrollToPosition(0);
             }
-
-
         }
     }
 
+    //the method for adding friends in friend fragment based on the button in the menu
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-
         if (id == R.id.add_friend_button) {
-            // do something here
+            //create the alert box
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             builder.setTitle("Friend Request? Add their Username below and zoom!");
 
@@ -168,8 +166,8 @@ public class FindFriendsFrag extends Fragment {
             input.setInputType(InputType.TYPE_CLASS_TEXT);
             builder.setView(input);
 
-            // Set up the buttons
-            builder.setPositiveButton("ZOOM", new DialogInterface.OnClickListener() {
+            // Used when current user presses the add button in the alert box
+            builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     friendAdd = input.getText().toString();
@@ -184,19 +182,18 @@ public class FindFriendsFrag extends Fragment {
                                         || userList.get(i).getUserId().equals(currUser.getUserId())) {
                                     flag = true;
                                 }
+                                //checks if the friend can be added to the friend list
                                 if (userList.get(i).getUserId().equals(friendAdd) && flag == false)
                                 {
                                     currUser.addFriend(friendAdd);
+                                    //sets new friends list into the database
                                     ref.child(currUser.getUserId()).child("friendList").setValue(currUser.getFriendList());
-                                    //ref.child(userID).child("friendList").setValue(currUser.getFriendList());
-                                    //create the ExampleItem and insert that into the friendsList
-                                    //create a new row for that friend
                                     added = 1;
                                     break;
                                 }
                             }
                         }
-
+                        //displays the correct toast message depending on if the person was added or not
                         if (added == 1) {
                             Toast.makeText(getActivity(), "Added : " + friendAdd, Toast.LENGTH_SHORT).show();
                             added = 0;
@@ -207,23 +204,26 @@ public class FindFriendsFrag extends Fragment {
                     dialog.cancel();
                 }
             });
+            //used when current user decides to stop the process of adding the friend
             builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     dialog.cancel();
                 }
             });
-
             builder.show();
         }
         return super.onOptionsItemSelected(item);
     }
 
+    //this method makes sure we are using the correct menu display on the top of the app
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
+        //select the correct menu layout
         inflater.inflate(R.menu.example_menu, menu);
 
+        //utilizes the search action
         MenuItem searchItem =  menu.findItem(R.id.action_search);
         final androidx.appcompat.widget.SearchView searchView = (androidx.appcompat.widget.SearchView) searchItem.getActionView();
         searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
